1c39ca494777a4f9b50ac46133af598a3a0110a4
NANAYAKKARA A.H.M. <92136486+harithmaduranga@users.noreply.github.com>
2025-02-28 09:14:00 +0530

    Update server.js  frontend url

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: "http://holidaylife.travel", // Your frontend
+  origin: "http://www.holidaylife.travel", // Your frontend
   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
 }));
